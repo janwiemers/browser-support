78ea1d4f365e4b6f3fab5f8587f7a7667b466a11
Jan Wiemers <j.wiemers@allerdesign.de>
2013-03-01 21:12:47 +0100

    Added a Demopage
    There is a Demopage to show the exetensibility

--- Browsersupport.js
+++ Browsersupport.js
@@ -194,31 +194,16 @@
                 return;
             }
 
+            if(test in this.extend || test in this.feature) {
+                throw 'A Test with this name is allready applyed';
+                return;
+            }
+            
             this.extend.push(test);
-
 
         }
 
     };
 })();
-
-var d = new Browsersupport();
-
-test = {
-    feature: 'csstransitions',
-    test   : function(){
-        var s = document.createElement('p').style, /*'s' for style. better to create an element if body yet to exist*/
-            v = ['ms','O','Moz','Webkit'];         /*'v' for vendor*/
-
-        if( s['transition'] == '' ) return true;   /*check first for prefeixed-free support*/
-        while( v.length )                          /*now go over the list of vendor prefixes and check support until one is found*/
-            if( v.pop() + 'Transition' in s )
-                return true;
-        return false;
-    }
-}
-
-d.add(test)
-d.detect();
 
 
